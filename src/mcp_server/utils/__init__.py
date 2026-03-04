599c12cf012c0a49e7d086934b047e0c6137266f
Mostafa Kamal <hiremostafa@gmail.com>
2026-03-04 13:37:14 +0600

    fix: remove unused register mcp component

--- src/mcp_server/utils/__init__.py
+++ src/mcp_server/utils/__init__.py
@@ -4,12 +4,10 @@ from mcp_server.utils.helpers import (
     validate_days,
     format_temperature,
 )
-from mcp_server.utils.register_mcp_components import register_mcp_components
 
 __all__ = [
     "format_date",
     "validate_days",
     "format_temperature",
-    "register_mcp_components",
 ]
 
